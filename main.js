01865dcf6918ddc2f318f5d57614c6b46250c18e
XanderSimon <rednax01@gmail.com>
2023-02-07 17:39:10 -0500

    all updates

---
+++ main.js
@@ -0,0 +1,26 @@
+let bg_color = "white";
+let txt_color = "black";
+let dark_mode = false;
+
+// function to toggle dark mode
+function toggle_dark_mode() {
+  // changes the toggle
+  dark_mode = !dark_mode; 
+
+  // if dark mode is enabled, change background to black and text to white
+  if (dark_mode) {
+    bg_color = "black";
+    txt_color = "white"
+    document.body.style.backgroundColor = bg_color;
+    document.body.style.color = txt_color;
+}
+  // otherwise change back to normal
+  else {
+    bg_color = "white";
+    txt_color = "black"
+    document.body.style.backgroundColor = bg_color;
+    document.body.style.color = txt_color;
+
+  }  
+
+}
